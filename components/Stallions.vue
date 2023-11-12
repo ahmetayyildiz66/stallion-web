587ecfa11de98ccc4d31a7de658e6121b067992c
ahmet <ahmet.ayyildiz@qlik.com>
2023-11-12 13:45:58 +0100

    feat: make the main page responsive

--- components/Stallions.vue
+++ components/Stallions.vue
@@ -1,19 +1,19 @@
 <template>
-  <section class="grid grid-cols-3 gap-5">
+  <section class="grid md:grid-cols-2 xl:grid-cols-3 gap-5">
     <div
       v-for="horse in getStallions()"
       :key="horse.id"
       class="border border-yellow-50 rounded-lg p-2 shadow-md shadow-gray-500 hover:cursor-pointer"
     >
-      <figure class="text-white grid grid-cols-2">
+      <figure class="text-white flex space-x-2 md:space-x-0 md:grid md:grid-cols-2 md:gap-2 gap-0">
         <NuxtImg :src="horse.image" class="border-none" />
 
-        <figcaption>
-          <h2 class="text-xl text-yellow-50">{{ horse.name }}</h2>
+        <figcaption class="space-y-1">
+          <h2 class="text-yellow-50 lg:text-lg 2xl:text-xl">{{ horse.name }}</h2>
           <span class="text-xs tracking-tight inline-block">{{
             horse.gen
           }}</span>
-          <span class="text-xs tracking-tight" v-if="horse.semen[0].price"
+          <span class="text-xs tracking-tight block" v-if="horse.semen[0].price"
             >€ {{ horse.semen[0].price }}</span
           >
         </figcaption>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="pt-6">
-    <div class="grid grid-cols-filter gap-10">
+    <div class="grid grid-cols-1 md:grid-cols-filter gap-10">
       <HorseFilters />
 
       <div class="space-y-12">
